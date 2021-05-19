76e1f4f980e5be8f6366d6d251804a1a5eac9430
Sii Yew Hei <syewhei@gmail.com>
2021-05-19 10:23:57 +0800

    navigation bar added

---
+++ MySekolah/app/src/main/java/com/example/mysekolah/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.mysekolah;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomePage();
+                    break;
+                case R.id.nav_notif:
+                    selectedFragment = new NotificationPage();
+                    break;
+                case R.id.nav_profile:
+                    selectedFragment = new ProfilePage();
+                    break;
+                case R.id.nav_search:
+                    selectedFragment = new SearchPage();
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return false;
+        }
+    };
+}
---
+++ MySekolah/app/src/main/java/com/example/mysekolah/NotificationPage.java
@@ -0,0 +1,20 @@
+package com.example.mysekolah;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotificationPage extends Fragment {
+
+    @Override
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notification, container, false);
+    }
+}
