4026308fd1a8c6163cb322a73bdf060538ba6b5c
avocadokk <konglokeyi99@gmail.com>
2021-05-20 13:25:50 +0800

    pre-school enroll

---
+++ MySekolah/app/src/main/java/com/example/mysekolah/PreSchoolForm.java
@@ -0,0 +1,14 @@
+package com.example.mysekolah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreSchoolForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_school_form);
+    }
+}
