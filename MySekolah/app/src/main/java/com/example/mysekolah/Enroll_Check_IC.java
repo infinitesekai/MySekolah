83228e04c21821dc4d97069b25f3bf323f50da37
avocadokk <konglokeyi99@gmail.com>
2021-05-20 20:37:19 +0800

    scrollview application form

--- MySekolah/app/src/main/java/com/example/mysekolah/Enroll_Check_IC.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/Enroll_Check_IC.java
@@ -4,12 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Enroll_Check_IC extends AppCompatActivity {
+
+    Button check;
+    EditText ic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,17 @@ public class Enroll_Check_IC extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        check= findViewById(R.id.btnCheck);
+
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(Enroll_Check_IC.this, PreSchoolForm.class);
+                startActivity(i);
+            }
+        });
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
--- MySekolah/app/src/main/java/com/example/mysekolah/PreSchoolForm.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/PreSchoolForm.java
@@ -1,14 +1,87 @@
 package com.example.mysekolah;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class PreSchoolForm extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class PreSchoolForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String[] state= {"Johor", "Kedah", "Kelantan", "Malacca", "Negeri Semnilan", "Pahang",
+            "Penang", "Perak", "Perlis", "Sabah", "Sarawak", "Selangor", "Terengganu", "Kuala Lumpur",
+            "Putarjaya", "Labuan"};
+
+    String[] district= {"Johor", "Kedah", "Kelantan", "Malacca", "Negeri Semnilan", "Pahang",
+            "Penang", "Perak", "Perlis", "Sabah", "Sarawak", "Selangor", "Terengganu", "Kuala Lumpur",
+            "Putarjaya", "Labuan"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_school_form);
+
+
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.spinnerState);
+        spin.setOnItemSelectedListener(this);
+
+        Spinner spin2 = (Spinner) findViewById(R.id.spinnerDistrict);
+        spin.setOnItemSelectedListener(this);
+
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,state);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
+        ArrayAdapter aa1 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,district);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin2.setAdapter(aa1);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomePage();
+                    break;
+                case R.id.nav_notif:
+                    selectedFragment = new NotificationPage();
+                    break;
+                case R.id.nav_profile:
+                    selectedFragment = new ProfilePage();
+                    break;
+                case R.id.nav_search:
+                    selectedFragment = new SearchPage();
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return false;
+        }
+    };
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
