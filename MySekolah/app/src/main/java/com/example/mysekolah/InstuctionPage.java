e5b70c8cc58a261eb341f125f96a95e13273f26a
Sii Yew Hei <syewhei@gmail.com>
2021-05-21 21:17:52 +0800

    adding scrolling, can continue to use for student side

---
+++ MySekolah/app/src/main/java/com/example/mysekolah/InstuctionPage.java
@@ -0,0 +1,14 @@
+package com.example.mysekolah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstuctionPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instuction_page);
+    }
+}
