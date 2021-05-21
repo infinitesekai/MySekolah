3a6c5300762f7c9407add547af099acfca511b67
Sii Yew Hei <syewhei@gmail.com>
2021-05-21 21:21:06 +0800

    rename the file from personlity_test_result to personality_test_home

--- MySekolah/app/src/main/java/com/example/mysekolah/PersonalityTestHome.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/PersonalityTestHome.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PersonalityTestResult extends AppCompatActivity {
+public class PersonalityTestHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personality_test_result);
+        setContentView(R.layout.activity_personality_test_home);
     }
 }
