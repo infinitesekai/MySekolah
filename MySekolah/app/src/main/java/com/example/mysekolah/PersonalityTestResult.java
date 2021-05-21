76053b10998ea724406532218e41832c8b5c09a2
Sii Yew Hei <syewhei@gmail.com>
2021-05-21 11:42:39 +0800

    starting of personality and career test's ui

---
+++ MySekolah/app/src/main/java/com/example/mysekolah/PersonalityTestResult.java
@@ -0,0 +1,14 @@
+package com.example.mysekolah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalityTestResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personality_test_result);
+    }
+}
