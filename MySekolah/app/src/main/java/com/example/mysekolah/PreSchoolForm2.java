45cd398c7ad37e9c90d8c37917d5a116f29da900
avocadokk <konglokeyi99@gmail.com>
2021-05-21 09:14:57 +0800

    pre school form

--- MySekolah/app/src/main/java/com/example/mysekolah/PreSchoolForm2.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/PreSchoolForm2.java
@@ -4,43 +4,30 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class PreSchoolForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class PreSchoolForm2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     String[] state= {"Johor", "Kedah", "Kelantan", "Malacca", "Negeri Semnilan", "Pahang",
             "Penang", "Perak", "Perlis", "Sabah", "Sarawak", "Selangor", "Terengganu", "Kuala Lumpur",
             "Putarjaya", "Labuan"};
 
-    String[] district= {"Johor", "Kedah", "Kelantan", "Malacca", "Negeri Semnilan", "Pahang",
-            "Penang", "Perak", "Perlis", "Sabah", "Sarawak", "Selangor", "Terengganu", "Kuala Lumpur",
-            "Putarjaya", "Labuan"};
-
-    Button next;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pre_school_form);
-
+        setContentView(R.layout.activity_pre_school_form2);
 
         //Getting the instance of Spinner and applying OnItemSelectedListener on it
         Spinner spin = (Spinner) findViewById(R.id.spinnerState);
         spin.setOnItemSelectedListener(this);
-
-        Spinner spin2 = (Spinner) findViewById(R.id.spinnerDistrict);
-        spin.setOnItemSelectedListener(this);
-
 
         //Creating the ArrayAdapter instance having the country list
         ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,state);
@@ -48,20 +35,6 @@ public class PreSchoolForm extends AppCompatActivity implements AdapterView.OnIt
         //Setting the ArrayAdapter data on the Spinner
         spin.setAdapter(aa);
 
-        ArrayAdapter aa1 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,district);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Setting the ArrayAdapter data on the Spinner
-        spin2.setAdapter(aa1);
-
-        next= findViewById(R.id.btnNext);
-
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i= new Intent(PreSchoolForm.this, PreSchoolForm2.class);
-                startActivity(i);
-            }
-        });
 
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
