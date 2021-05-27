9e8a050555ea0f14b331689a408ca12eed9abb0b
Sii Yew Hei <syewhei@gmail.com>
2021-05-28 01:35:51 +0800

    connect to the relative activities

--- MySekolah/app/src/main/java/com/example/mysekolah/HomePage_Student.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/HomePage_Student.java
@@ -1,2 +1,54 @@
-package com.example.mysekolah;public class HomePage_Student {
+package com.example.mysekolah;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
+public class HomePage_Student extends Fragment implements View.OnClickListener {
+
+    public CardView academic_qualification, exam_check, personality_check;
+
+    @Override
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_home_student, container, false);
+
+        academic_qualification = (CardView)v.findViewById(R.id.academic_qualification);
+        exam_check = (CardView)v.findViewById(R.id.exam_check);
+        personality_check = (CardView)v.findViewById(R.id.personality_test);
+
+        academic_qualification.setOnClickListener(this);
+        exam_check.setOnClickListener(this);
+        personality_check.setOnClickListener(this);
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.academic_qualification:
+                i= new Intent(getActivity(), Academic_Qualification.class);
+                startActivity(i);
+                break;
+            case R.id.exam_check:
+                i = new Intent(getActivity(), ExamResultTable.class);
+                startActivity(i);
+                break;
+            case R.id.personality_test:
+                i=new Intent(getActivity(), PersonalityTestHome.class);
+                startActivity(i);
+
+        }
+    }
 }
