8d370f699ee699569ba37c3ba03c60ed52864c4a
LY <infinitesekai@gmail.com>
2021-05-29 11:24:31 +0800

    merge yilin files

---
+++ MySekolah/app/src/main/java/com/example/mysekolah/Introductory_Page.java
@@ -0,0 +1,14 @@
+package com.example.mysekolah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Introductory_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introductory_page);
+    }
+}
