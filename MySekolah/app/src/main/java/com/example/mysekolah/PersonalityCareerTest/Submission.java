dcb54978f6d1b3873824b75a79f4dff7b0c4db58
Sii Yew Hei <syewhei@gmail.com>
2021-06-02 21:10:26 +0800

    add in the pop up and linked to the last question

--- MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/Submission.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/Submission.java
@@ -2,13 +2,31 @@ package com.example.mysekolah.PersonalityCareerTest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
 
-public class Submission extends AppCompatActivity {
+import com.example.mysekolah.R;
+
+public class Submission extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*.8), (int) (height*.4));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+        getWindow().setAttributes(params);
     }
 }
