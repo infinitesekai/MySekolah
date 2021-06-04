57a253d1949d083894fe593b7aa3ac361744343b
Sii Yew Hei <syewhei@gmail.com>
2021-06-05 04:43:29 +0800

    add function to load the dialog

--- MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/LoadingDialog.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/LoadingDialog.java
@@ -1,2 +1,34 @@
-package com.example.mysekolah.PersonalityCareerTest;public class LoadingDialog {
+package com.example.mysekolah.PersonalityCareerTest;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.LayoutInflater;
+
+import com.example.mysekolah.R;
+
+public class LoadingDialog {
+
+    Activity activity;
+    AlertDialog dialog;
+
+    //constructor
+    LoadingDialog(Activity myActivity) {
+        activity = myActivity;
+    }
+
+    void startLoadingDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+        LayoutInflater inflater = activity.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.custom_progress, null));
+        builder.setCancelable(false);
+
+        dialog = builder.create();
+        dialog.show();
+    }
+
+    void dismissDialog(){
+        dialog.dismiss();
+    }
 }
+
