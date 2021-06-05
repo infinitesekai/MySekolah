1511924dd45aff9a856e7d495ddd588c13f95495
Sii Yew Hei <syewhei@gmail.com>
2021-06-05 12:17:17 +0800

    trying to save the state of radio button

--- MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/PersonalTestQuestion.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/PersonalTestQuestion.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -32,7 +34,7 @@ public class PersonalTestQuestion extends AppCompatActivity {
     private Question currentQuestion;
 
     //check if the question is answered or not
-    private boolean answered;
+    private boolean answered = true;
 
     @SuppressLint("ResourceAsColor")
     @Override
@@ -43,9 +45,13 @@ public class PersonalTestQuestion extends AppCompatActivity {
         tvQuestion = findViewById(R.id.txt_question);
         tvQuestionCount = findViewById(R.id.txt_numbering);
         tvQuestionNoGuide = findViewById(R.id.total_question);
+
         rbGroup = findViewById(R.id.radio_group);
         rb1 = findViewById(R.id.radioBtn_agree);
         rb2 = findViewById(R.id.radioBtn_disagree);
+        rb1.setChecked(Update("option1"));
+        rb2.setChecked(Update("option2"));
+
         buttonNext = findViewById(R.id.btn_next);
         buttonBack = findViewById(R.id.btn_back);
 
@@ -58,6 +64,8 @@ public class PersonalTestQuestion extends AppCompatActivity {
 
         //random the questions in the question collection
         Collections.shuffle(questionList);
+
+        rbGroup.clearCheck();
 
         //show the first question
         showNextQuestion();
@@ -84,25 +92,29 @@ public class PersonalTestQuestion extends AppCompatActivity {
         });
 
         buttonBack.setOnClickListener(new View.OnClickListener() {
+
             @SuppressLint({"SetTextI18n"})
             @Override
             public void onClick(View v) {
-                if(questionCounter>0) {
-
+                if (questionCounter > 0) {
+                    // decrease the current question counter to back to the previous question
                     questionCounter--;
                     showNextQuestion();
-                     // here you decrement
-                }
-                else {
+                } else {
                     // you're at the first question => no previous one
+                    Toast.makeText(
+                            PersonalTestQuestion.this,
+                            "You are on the first question already.",
+                            Toast.LENGTH_SHORT
+                    ).show();
                 }
             }
         });
+
     }
 
     @SuppressLint({"SetTextI18n"})
     private void showNextQuestion() {
-
         if ((questionCounter-1)>=0){
             buttonBack.setBackgroundResource(R.drawable.rounded_btn_black);
             buttonBack.setTextColor(Color.WHITE);
@@ -111,6 +123,22 @@ public class PersonalTestQuestion extends AppCompatActivity {
             buttonBack.setBackgroundResource(R.drawable.rounded_btn_grey);
             buttonBack.setTextColor(Color.BLACK);
         }
+
+//        rb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean one_isChecked) {
+//                SaveIntoPref("option1", one_isChecked);
+//            }
+//        });
+//
+//        rb2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean two_isChecked) {
+//                SaveIntoPref("option2", two_isChecked);
+//            }
+//        });
 
         //if still have questions to answer
         if (questionCounter < questionCountTotal) {
@@ -136,4 +164,18 @@ public class PersonalTestQuestion extends AppCompatActivity {
         Intent i = new Intent(PersonalTestQuestion.this, Submission.class);
         startActivity(i);
     }
+
+
+    private void SaveIntoPref(String key, boolean value){
+        SharedPreferences sp = getSharedPreferences("option", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    private boolean Update(String key){
+        SharedPreferences sp = getSharedPreferences("option", MODE_PRIVATE);
+        return sp.getBoolean(key, false);
+    }
+
 }
