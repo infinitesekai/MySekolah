a2842982e34b5bb903f85349926208b0e4d181e5
Sii Yew Hei <syewhei@gmail.com>
2021-06-05 15:11:55 +0800

    add the navigation bar

--- MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/PersonalityTestHome.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/PersonalityTestHome.java
@@ -1,14 +1,24 @@
 package com.example.mysekolah.PersonalityCareerTest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.example.mysekolah.HomePage;
+import com.example.mysekolah.HomePage_Student;
+import com.example.mysekolah.NotificationPage;
+import com.example.mysekolah.ProfilePage;
 import com.example.mysekolah.R;
+import com.example.mysekolah.SearchPage;
+import com.example.mysekolah.User;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PersonalityTestHome extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,6 +29,9 @@ public class PersonalityTestHome extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personality_test_home);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
         take_test = findViewById(R.id.takeTestCard);
         test_result = findViewById(R.id.checkResultCard);
 
@@ -26,6 +39,31 @@ public class PersonalityTestHome extends AppCompatActivity implements View.OnCli
         test_result.setOnClickListener(this);
 
     }
+    //navigation bar
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomePage_Student();
+                    break;
+                case R.id.nav_notif:
+                    selectedFragment = new NotificationPage();
+                    break;
+                case R.id.nav_profile:
+                    selectedFragment = new ProfilePage();
+                    break;
+                case R.id.nav_search:
+                    selectedFragment = new SearchPage();
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return false;
+        }
+    };
+
 
     @SuppressLint("NonConstantResourceId")
     @Override
