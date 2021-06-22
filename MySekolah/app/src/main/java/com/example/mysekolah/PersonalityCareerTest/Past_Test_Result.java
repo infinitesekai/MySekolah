2f4aa3baa6112851cf210dfcef9cfc058e772060
Sii Yew Hei <syewhei@gmail.com>
2021-06-23 07:54:50 +0800

    add function export and tidy code

--- MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/Past_Test_Result.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/Past_Test_Result.java
@@ -1,11 +1,23 @@
 package com.example.mysekolah.PersonalityCareerTest;
 
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.example.mysekolah.BuildConfig;
 import com.example.mysekolah.DatabaseAccess;
+import com.example.mysekolah.Export_Qualification;
 import com.example.mysekolah.HomePage_Student;
 import com.example.mysekolah.ProfilePage;
 import com.example.mysekolah.R;
@@ -16,18 +28,29 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +63,10 @@ public class Past_Test_Result extends AppCompatActivity implements View.OnClickL
     TextView H1;
     TextView H2;
     TextView H3;
+
+    FrameLayout ll_scroll;
+    private Bitmap bitmap;
+    Button result_export;
 
     private User currentUser;
     private int lastfragment;
@@ -63,11 +90,21 @@ public class Past_Test_Result extends AppCompatActivity implements View.OnClickL
     TextView sug3;
     TextView name;
     TextView testeric;
+    public static final int EXTERNAL_STORAGE_REQ_CODE = 10 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_test_result);
+
+        int permission = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // request permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    EXTERNAL_STORAGE_REQ_CODE);
+        }
 
         currentUser = (User) getIntent().getSerializableExtra("user");
         lastfragment = 0;
@@ -115,7 +152,8 @@ public class Past_Test_Result extends AppCompatActivity implements View.OnClickL
         cardView3 = findViewById(R.id.result_card3_past);
 
         result_quit = findViewById(R.id.result_quit_past);
-
+        result_export = findViewById(R.id.result_export);
+        ll_scroll = findViewById(R.id.ll_scroll);
 
         name.setText(currentUser.getName());
         testeric.setText(currentUser.getICNo());
@@ -221,6 +259,8 @@ public class Past_Test_Result extends AppCompatActivity implements View.OnClickL
         }
 
         result_quit.setOnClickListener(this);
+        result_export.setOnClickListener(this);
+
 
     }
 
@@ -309,7 +349,106 @@ public class Past_Test_Result extends AppCompatActivity implements View.OnClickL
                 startActivity(intent);
                 break;
 
+            case R.id.result_export:
+                Log.d("size"," "+ll_scroll.getWidth() +"  "+ll_scroll.getWidth());
+                bitmap = loadBitmapFromView(ll_scroll, ll_scroll.getWidth(), ll_scroll.getHeight());
+                createPdf();
+        }
+    }
+    public static Bitmap loadBitmapFromView(View v, int width, int height) {
+        Bitmap b = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.draw(c);
 
+        return b;
+    }
+
+    private void createPdf(){
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        //  Display display = wm.getDefaultDisplay();
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        float hight = displaymetrics.heightPixels ;
+        float width = displaymetrics.widthPixels ;
+
+        int convertHighet = (int) hight, convertWidth = (int) width;
+
+//        Resources mResources = getResources();
+//        Bitmap bitmap = BitmapFactory.decodeResource(mResources, R.drawable.screenshot);
+
+        //start first page
+        PdfDocument document = new PdfDocument();
+        int page_number=2;
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(convertWidth, convertHighet, page_number).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+
+        Paint paint = new Paint();
+        canvas.drawPaint(paint);
+
+        bitmap = Bitmap.createScaledBitmap(bitmap, convertWidth, convertHighet, true);
+
+        paint.setColor(Color.BLUE);
+        canvas.drawBitmap(bitmap, 0, 0 , null);
+        document.finishPage(page);
+
+
+        //Create a directory for your PDF
+//        File pdfDir = new File(Environment.getExternalStoragePublicDirectory(
+//                Environment.DIRECTORY_DOCUMENTS), "MyApp");
+//        if (!pdfDir.exists()){
+//            pdfDir.mkdir();
+//        }
+        // write the document content
+        String targetPdf = "/sdcard/result.pdf";
+
+//        String targetPdf = "/storage/emulated/0/Download/pdffromScroll.pdf";
+        File filePath;
+        filePath = new File(targetPdf);
+        try {
+            document.writeTo(new FileOutputStream(filePath));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Something wrong: " + e.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        // close the document
+        document.close();
+        Toast.makeText(this, "PDF of Scroll is created!!!", Toast.LENGTH_SHORT).show();
+
+        openGeneratedPDF();
+
+
+    }
+
+
+
+    private void openGeneratedPDF(){
+        File file = new File("/sdcard/result.pdf");
+//        File file = new File("/storage/emulated/0/Download/pdffromScroll.pdf");
+
+        if (file.exists())
+        {
+            Intent intent=new Intent(Intent.ACTION_VIEW);
+//            Uri uri = Uri.fromFile(file);
+//            Context context= Past_Test_Result.this;
+//            Uri uri = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".provider", file);
+
+            Uri uri = FileProvider.getUriForFile(Past_Test_Result.this, BuildConfig.APPLICATION_ID + ".provider",file);
+
+            intent.setDataAndType(uri, "application/pdf");
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            try
+            {
+                startActivity(intent);
+            }
+            catch(ActivityNotFoundException e)
+            {
+                Toast.makeText(Past_Test_Result.this, "No Application available to view pdf", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
--- MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/ResultPersonalityTest.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/PersonalityCareerTest/ResultPersonalityTest.java
@@ -2,11 +2,19 @@ package com.example.mysekolah.PersonalityCareerTest;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.example.mysekolah.BuildConfig;
 import com.example.mysekolah.DatabaseAccess;
 import com.example.mysekolah.HomePage_Student;
 import com.example.mysekolah.MainActivity;
@@ -20,19 +28,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -47,6 +63,11 @@ public class ResultPersonalityTest extends AppCompatActivity implements View.OnC
     LinearLayout expandable_view, expandable_view2, expandable_view3;
     ImageView imageView, imageView2, imageView3;
     CardView cardView, cardView2, cardView3;
+
+    FrameLayout ll_scroll_now;
+    private Bitmap bitmap;
+    Button result_export_now;
+
     Button result_quit;
     TextView Rvalue;
     TextView Ivalue;
@@ -104,11 +125,45 @@ public class ResultPersonalityTest extends AppCompatActivity implements View.OnC
 
         result_quit.setOnClickListener(this);
         image_btn.setOnClickListener(this);
+        result_export_now.setOnClickListener(this);
 
 
     }
 
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomePage_Student();
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("user",currentUser);//这里的values就是我们要传的值
+                    selectedFragment.setArguments(bundle);
+                    lastfragment = R.id.nav_home;
+                    break;
+
+                case R.id.nav_profile:
+                    selectedFragment = new ProfilePage();
+                    bundle = new Bundle();
+                    bundle.putSerializable("user",currentUser);//这里的values就是我们要传的值
+                    selectedFragment.setArguments(bundle);
+                    //lastfragment = R.id.nav_profile;
+                    break;
+                case R.id.nav_search:
+                    selectedFragment = new SearchPage_Student();
+                    bundle = new Bundle();
+                    bundle.putSerializable("user",currentUser);//这里的values就是我们要传的值
+                    selectedFragment.setArguments(bundle);
+                    lastfragment = R.id.nav_search;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return false;
+        }
+    };
 
     //expand and show less 
     public void showmore(View view){
@@ -206,6 +261,8 @@ public class ResultPersonalityTest extends AppCompatActivity implements View.OnC
         cardView3 = findViewById(R.id.result_card3);
 
         result_quit = findViewById(R.id.result_quit);
+        result_export_now = findViewById(R.id.result_export_now);
+        ll_scroll_now = findViewById(R.id.ll_scroll_now);
 
 
         image_btn = findViewById(R.id.imageButton);
@@ -402,6 +459,10 @@ public class ResultPersonalityTest extends AppCompatActivity implements View.OnC
                 intent.putExtra("ICNo", currentUser.getICNo());
                 startActivity(intent);
                 break;
+            case R.id.result_export_now:
+                Log.d("size"," "+ll_scroll_now.getWidth() +"  "+ll_scroll_now.getWidth());
+                bitmap = loadBitmapFromView(ll_scroll_now, ll_scroll_now.getWidth(), ll_scroll_now.getHeight());
+                createPdf();
 
             case  R.id.imageButton:
                 intent = new Intent(this, ResultInfo.class);
@@ -410,39 +471,101 @@ public class ResultPersonalityTest extends AppCompatActivity implements View.OnC
         }
     }
 
+    public static Bitmap loadBitmapFromView(View v, int width, int height) {
+        Bitmap b = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.draw(c);
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return b;
+    }
 
-            Fragment selectedFragment = null;
+    private void createPdf(){
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        //  Display display = wm.getDefaultDisplay();
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        float hight = displaymetrics.heightPixels ;
+        float width = displaymetrics.widthPixels ;
 
-            switch (item.getItemId()) {
-                case R.id.nav_home:
-                    selectedFragment = new HomePage_Student();
-                    Bundle bundle = new Bundle();
-                    bundle.putSerializable("user",currentUser);//这里的values就是我们要传的值
-                    selectedFragment.setArguments(bundle);
-                    lastfragment = R.id.nav_home;
-                    break;
+        int convertHighet = (int) hight, convertWidth = (int) width;
 
-                case R.id.nav_profile:
-                    selectedFragment = new ProfilePage();
-                    bundle = new Bundle();
-                    bundle.putSerializable("user",currentUser);//这里的values就是我们要传的值
-                    selectedFragment.setArguments(bundle);
-                    //lastfragment = R.id.nav_profile;
-                    break;
-                case R.id.nav_search:
-                    selectedFragment = new SearchPage_Student();
-                    bundle = new Bundle();
-                    bundle.putSerializable("user",currentUser);//这里的values就是我们要传的值
-                    selectedFragment.setArguments(bundle);
-                    lastfragment = R.id.nav_search;
-            }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-            return false;
+//        Resources mResources = getResources();
+//        Bitmap bitmap = BitmapFactory.decodeResource(mResources, R.drawable.screenshot);
+
+        //start first page
+        PdfDocument document = new PdfDocument();
+        int page_number=2;
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(convertWidth, convertHighet, page_number).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        Canvas canvas = page.getCanvas();
+
+        Paint paint = new Paint();
+        canvas.drawPaint(paint);
+
+        bitmap = Bitmap.createScaledBitmap(bitmap, convertWidth, convertHighet, true);
+
+        paint.setColor(Color.BLUE);
+        canvas.drawBitmap(bitmap, 0, 0 , null);
+        document.finishPage(page);
+
+
+        //Create a directory for your PDF
+//        File pdfDir = new File(Environment.getExternalStoragePublicDirectory(
+//                Environment.DIRECTORY_DOCUMENTS), "MyApp");
+//        if (!pdfDir.exists()){
+//            pdfDir.mkdir();
+//        }
+        // write the document content
+        String targetPdf = "/sdcard/result_current.pdf";
+
+//        String targetPdf = "/storage/emulated/0/Download/pdffromScroll.pdf";
+        File filePath;
+        filePath = new File(targetPdf);
+        try {
+            document.writeTo(new FileOutputStream(filePath));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Something wrong: " + e.toString(), Toast.LENGTH_LONG).show();
         }
-    };
+
+        // close the document
+        document.close();
+        Toast.makeText(this, "PDF of Scroll is created!!!", Toast.LENGTH_SHORT).show();
+
+        openGeneratedPDF();
+
+
+    }
+
+
+
+    private void openGeneratedPDF(){
+        File file = new File("/sdcard/result_current.pdf");
+//        File file = new File("/storage/emulated/0/Download/pdffromScroll.pdf");
+
+        if (file.exists())
+        {
+            Intent intent=new Intent(Intent.ACTION_VIEW);
+//            Uri uri = Uri.fromFile(file);
+//            Context context= Past_Test_Result.this;
+//            Uri uri = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".provider", file);
+
+            Uri uri = FileProvider.getUriForFile(ResultPersonalityTest.this, BuildConfig.APPLICATION_ID + ".provider",file);
+
+            intent.setDataAndType(uri, "application/pdf");
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            try
+            {
+                startActivity(intent);
+            }
+            catch(ActivityNotFoundException e)
+            {
+                Toast.makeText(ResultPersonalityTest.this, "No Application available to view pdf", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
 }
