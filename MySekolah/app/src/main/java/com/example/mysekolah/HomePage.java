00f43001bf8cd247fc68e5c49886053c6e7219b3
Sii Yew Hei <syewhei@gmail.com>
2021-06-25 09:46:11 +0800

    add comments about video.

--- MySekolah/app/src/main/java/com/example/mysekolah/HomePage.java
+++ MySekolah/app/src/main/java/com/example/mysekolah/HomePage.java
@@ -49,6 +49,7 @@ public class HomePage extends Fragment implements View.OnClickListener {
 
         //set the video
         videoView = (VideoView) v.findViewById(R.id.news);
+        //set the video path that in the raw folder
         String videoPath =
                 "android.resource://" +
                         Objects.requireNonNull(getActivity()).getPackageName() +
@@ -57,10 +58,13 @@ public class HomePage extends Fragment implements View.OnClickListener {
         Uri uri = Uri.parse(videoPath);
         videoView.setVideoURI(uri);
 
+        //Mediacontroller contains the buttons like "Play/Pause"that we need
         MediaController mediaController = new MediaController(getActivity());
+        //set the video to the media controller
         videoView.setMediaController(mediaController);
+        //set the video to start automatically
         videoView.start();
-
+        //set the video a callback to be invoked, take a listener as parameter.
         videoView.setOnPreparedListener(this::PreparedListener);
 
         //on click listener on card view
@@ -72,24 +76,39 @@ public class HomePage extends Fragment implements View.OnClickListener {
 
     }
 
+    //the listener parameter of video
     private void PreparedListener(MediaPlayer mp) {
         try {
+            //mute the volume of the video when the user enter the page
             mp.setVolume(0f, 0f);
+            //if mute volume is true, the mute icon is shown
             if (!mVolumePlaying) {
                 volume.setImageResource(R.drawable.ic_baseline_volume_off_24);
             }
+
+            //set the looping of video on
             mp.setLooping(true);
             mp.start();
+
+            //user can click on the image to change the volume (mute or unmute)
             volume.setOnClickListener(v -> {
+
+                //if mute is false, and the icon is clicked
+                // means the user want to mute the volume, so mute icon is shown
                 if (mVolumePlaying) {
-                    Log.d(TAG, "setVolume OFF");
+                    Log.d(TAG, "setVolume OFF"); //text show in console to double check
                     volume.setImageResource(R.drawable.ic_baseline_volume_off_24);
                     mp.setVolume(0F, 0F);
-                } else {
-                    Log.d(TAG, "setVolume ON");
+                }
+                //else if mute is true, and the icon is clicked
+                // means the user want to unmute the volume, so unmute icon is shown
+                else {
+                    Log.d(TAG, "setVolume ON"); //text show in console to double check
                     volume.setImageResource(R.drawable.ic_baseline_volume_up_24);
                     mp.setVolume(1F, 1F);
                 }
+                //when the clicking is finished, set the mVolumePlaying to opposite (due to the if else above)
+                //so the the difference can be observed the click is functional
                 mVolumePlaying = !mVolumePlaying;
             });
         } catch (Exception e) {
